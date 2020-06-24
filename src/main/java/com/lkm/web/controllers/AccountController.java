66e9312817dbf058f3abe5c4bd0d569cd53b3c4e
임경민 <60154655+limup3@users.noreply.github.com>
2020-06-24 16:07:50 +0900

    0624
    0624

--- src/main/java/com/lkm/web/controllers/AccountController.java
+++ src/main/java/com/lkm/web/controllers/AccountController.java
@@ -3,53 +3,63 @@ package com.lkm.web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lkm.web.domains.AccountDTO;
 import com.lkm.web.mappers.AccountMapper;
 import com.lkm.web.services.AccountService;
-import com.lkm.web.services.AccountServiceImpl;
+//import com.lkm.web.services.AccountServiceImpl;
 
 import lombok.Data;
 
 @CrossOrigin(origins="*", allowedHeaders = "*")
 @RestController
 @Data
+@RequestMapping("/account")
 public class AccountController {
-
-	@Autowired AccountDTO account;
-	@Autowired AccountMapper accountMapper;
-	@Autowired AccountService accountService;
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@PostMapping("/signup")
-	public void signup(@RequestBody String total, String id, String name, String password) {
-		account.setId(id);
-		account.setName(name);
-		account.setPassword(password);
-		accountMapper.insertMember(account);
+	@GetMapping("/join/form")
+	public String joinForm() {
+		logger.info("Ïù¥Îèô ÌôîÎ©¥ ÏßÑÏûÖ");
+		return "Join";
 	}
-	
-	@PostMapping("/login")
-	public Map<String, Object> login(@RequestBody AccountDTO params) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		account = accountService.login(params);
-		 if(account != null){
-	            System.out.println("∑Œ±◊¿Œ¡§∫∏" + account.toString());
-	            map.put("result", true);
-	        }else{
-	            map.put("result", false);
-	        }
-	        System.out.println("æ∆¿Ãµ" + account.toString());
-	        System.out.println("∫Òπ–π¯»£" + params.getPassword());
-
-
-
-	        map.put("account",account);
-	        return map;
-	}
+//	@Autowired AccountDTO account;
+//	@Autowired AccountMapper accountMapper;
+//	@Autowired AccountService accountService;
+//	
+//	@PostMapping("/signup")
+//	public void signup(@RequestBody String total, String id, String name, String password) {
+//		account.setId(id);
+//		account.setName(name);
+//		account.setPassword(password);
+//		accountMapper.insertMember(account);
+//	}
+//	
+//	@PostMapping("/login")
+//	public Map<String, Object> login(@RequestBody AccountDTO params) {
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		account = accountService.login(params);
+//		 if(account != null){
+//	            System.out.println("ÔøΩŒ±ÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩ" + account.toString());
+//	            map.put("result", true);
+//	        }else{
+//	            map.put("result", false);
+//	        }
+//	        System.out.println("ÔøΩÔøΩÔøΩÃµÔøΩ" + account.toString());
+//	        System.out.println("ÔøΩÔøΩ–πÔøΩ»£" + params.getPassword());
+//
+//
+//
+//	        map.put("account",account);
+//	        return map;
+//	}
 }
--- src/main/java/com/lkm/web/controllers/HomeController.java
+++ src/main/java/com/lkm/web/controllers/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		model.addAttribute("serverTime", DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale).format(new Date()) );
 		
-		return "home";
+		return "Home";
 	}
 	
 }
